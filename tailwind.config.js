dd0ad09335e4264edf475be8ed9d65a612673662
Dushmanta <dushmanta0511@gmail.com>
2025-07-01 11:28:50 +0530

    style: update primary color, convert some styles to tailwind, and add markdown element styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,38 +2,36 @@
 
 const colors = {
   gray: {
-    0: '#090b11',
-    50: '#141925',
-    100: '#283044',
-    200: '#3d4663',
-    300: '#505d84',
-    400: '#6474a2',
-    500: '#8490b5',
-    600: '#a3acc8',
-    700: '#c3cadb',
-    800: '#e3e6ee',
-    900: '#f3f4f7',
-    '999-basis': '0, 0%, 100%',
-    '999_40': 'hsla(0, 0%, 100%, 0.4)',
-    999: '#ffffff',
+    0: 'var(--gray-0)',
+    50: 'var(--gray-50)',
+    100: 'var(--gray-100)',
+    200: 'var(--gray-200)',
+    300: 'var(--gray-300)',
+    400: 'var(--gray-400)',
+    500: 'var(--gray-500)',
+    600: 'var(--gray-600)',
+    700: 'var(--gray-700)',
+    800: 'var(--gray-800)',
+    900: 'var(--gray-900)',
+    999: 'var(--gray-999)',
+    '999_40': 'var(--gray-999_40)',
   },
   accent: {
-    light: '#69dcff',
-    regular: '#01c2fd',
-    dark: '#1c0056',
-    overlay: 'hsla(194, 89%, 67%, 0.33)',
-    subtleOverlay: 'hsla(194, 89%, 67%, 0.33)',
-    textOver: '#ffffff',
+    light: 'var(--accent-light)',
+    regular: 'var(--accent-regular)',
+    dark: 'var(--accent-dark)',
+    overlay: 'var(--accent-overlay)',
+    textOver: 'var(--accent-text-over)',
   },
   link: {
-    color: '#01c2fd',
+    color: 'var(--accent-regular)',
   },
 };
 
 const gradients = {
   subtle: 'linear-gradient(150deg, #f3f4f7 19%, #ffffff 150%)',
-  accent: 'linear-gradient(150deg, #69dcff, #01c2fd, #1c0056)',
-  accentCta: 'linear-gradient(150deg, #01c2fd, #1c0056)',
+  accent: 'linear-gradient(150deg, #69dcff, #7f6df2, #1c0056)',
+  accentCta: 'linear-gradient(150deg, #7f6df2, #1c0056)',
   stroke: 'linear-gradient(180deg, #f3f4f7, #c3cadb)',
 };
 
@@ -55,6 +53,49 @@ const fontSize = {
   '5xl': '4.5rem',
 };
 
+// Dark mode
+const darkThemeColors = {
+  gray: {
+    0: '#ffffff',
+    50: '#f3f4f7',
+    100: '#e3e6ee',
+    200: '#c3cadb',
+    300: '#a3acc8',
+    400: '#8490b5',
+    500: '#6474a2',
+    600: '#505d84',
+    700: '#3d4663',
+    800: '#283044',
+    900: '#141925',
+    '999-basis': '225, 31%, 5%',
+    '999_40': 'hsla(225, 31%, 5%, 0.4)',
+    999: '#090b11',
+  },
+  accent: {
+    light: '#1c0056',
+    regular: '#7f6df2',
+    dark: '#7f6df2',
+    overlay: '#d3cef2',
+    subtleOverlay: '#3e2da8',
+    textOver: '#ffffff',
+  },
+  link: {
+    color: '#1c0056',
+  },
+};
+
+const darkThemeGradients = {
+  subtle: 'linear-gradient(150deg, #141925 19%, #090b11 81%)',
+  accentCta: 'linear-gradient(150deg, #1c0056, #7f6df2)',
+  stroke: 'linear-gradient(180deg, #505d84, #283044)',
+};
+
+const darkThemeShadows = {
+  sm: '0px 6px 3px rgba(255, 255, 255, 0.01), 0px 4px 2px rgba(255, 255, 255, 0.01), 0px 2px 2px rgba(255, 255, 255, 0.02), 0px 0px 1px rgba(255, 255, 255, 0.03)',
+  md: '0px 28px 11px rgba(255, 255, 255, 0.01), 0px 16px 10px rgba(255, 255, 255, 0.03), 0px 7px 7px rgba(255, 255, 255, 0.05), 0px 2px 4px rgba(255, 255, 255, 0.06)',
+  lg: '0px 62px 25px rgba(255, 255, 255, 0.01), 0px 35px 21px rgba(255, 255, 255, 0.05), 0px 16px 16px rgba(255, 255, 255, 0.1), 0px 4px 9px rgba(255, 255, 255, 0.12)',
+};
+
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
@@ -65,7 +106,13 @@ export default {
       backgroundImage: {
         ...gradients,
       },
+      dark: {
+        colors: darkThemeColors,
+        backgroundImage: darkThemeGradients,
+        boxShadow: darkThemeShadows,
+      },
     },
   },
+  darkMode: ['class', '[class="theme-dark"]'],
   plugins: [],
 };
