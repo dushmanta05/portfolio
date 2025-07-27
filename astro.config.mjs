a0b1b39ded730e1f224150505d95a67662977b2e
Dushmanta <dushmanta0511@gmail.com>
2025-07-27 18:40:17 +0530

    chore: update import aliases

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,10 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   integrations: [tailwind()],
+  tsconfig: './tsconfig.json',
   markdown: {
     shikiConfig: {
-      theme: "one-dark-pro",
+      theme: 'one-dark-pro',
       wrap: true,
       langs: [],
     },
