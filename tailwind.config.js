b9b4c169f8f53d6409bacdc5cc8116d3b559c9e1
Dushmanta <dushmanta0511@gmail.com>
2025-08-03 20:46:51 +0530

    feat: add animated chevron-down icon to indicate scroll

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,6 +40,15 @@ export default {
       screens: {
         sm: '800px',
       },
+      animation: {
+        fadeIn: 'fadeIn 1s ease-out',
+      },
+      keyframes: {
+        fadeIn: {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
     },
   },
   darkMode: ['class', '[class="theme-dark"]'],
